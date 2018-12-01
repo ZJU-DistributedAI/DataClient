728b7266ad4ec27ac10153f04dc8265ba1bb8415
ItemZheng <3160102332@zju.edu.cn>
2018-12-01 22:20:32 +0800

    with transaction

--- data_client.go
+++ data_client.go
@@ -23,7 +23,7 @@ func NewDataClientController(service *goa.Service) *DataClientController {
 }
 
 
-var keyStoreDir 	= "./UTC--2018-12-01T11-38-48.251200293Z--eba4ad35b4cd108c713b7aabdce5b0f196847464"
+var keyStoreDir 	= "UTC--2018-12-01T11-00-43.186381425Z--0d3bf5d5e8101cf0f49f2c0013f470d39527d01b"
 var ETH_HOST 		= "http://47.52.163.119:8545"
 var password  		= "   "
 
@@ -36,8 +36,8 @@ func (c *DataClientController) Add(ctx *app.AddDataClientContext) error {
 
 	var to = "3dfd86891ea4a634e4a6e1c8e75d1a92a0928346"
 	value := big.NewInt(0)
-	gasPrice := big.NewInt(234567897654321)
-	gasLimit := uint64(2000000)
+	gasPrice := big.NewInt(2000000000)
+	gasLimit := uint64(2711301)
 	data := "add " + ctx.Hash
 
 	transactionHash, err := sendTransaction(to, gasLimit, gasPrice, value, data)
@@ -76,25 +76,26 @@ func sendTransaction(to string, gasLimite uint64, gasPirce *big.Int, value *big.
 
 	signature, signatureErr := crypto.Sign(tx.Hash().Bytes(), fromPrivkey)
 	if signatureErr != nil {
-		fmt.Println("signature create error:")
+		fmt.Println("signature create error")
 		return "",signatureErr
 	}
 
 	signedTx, signErr := tx.WithSignature(types.HomesteadSigner{}, signature)
 	if signErr != nil{
-		fmt.Println("signature create error:")
+		fmt.Println("signature create error")
 		return "",signErr
 	}
 
 	client, err := ethclient.Dial(ETH_HOST)
 	if err != nil {
-		fmt.Println("client connection error:")
+		fmt.Println("client connection error")
 		return "",err
 	}
 
 	txErr := client.SendTransaction(context.Background(), signedTx)
 	if txErr != nil {
-		fmt.Println("send tx error:")
+		fmt.Println("send tx error")
+		panic(txErr)
 		return "",txErr
 	}
 	return signedTx.Hash().String(), nil
